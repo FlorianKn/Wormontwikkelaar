bfb758c80cc43ee1bf0cfffb5d63d0b4cdc99748
FlorianKn <floknoerzer@web.de>
2018-10-30 15:20:49 +0100

    Add some mock data and initialize warehouse

---
+++ src/app/mock-part.ts
@@ -0,0 +1,7 @@
+import { Part } from './part';
+
+export const PARTLIST: Part[] = [
+    { id: 1, name: 'Schrauben', description:'10er' },
+    { id: 2, name: 'Säge', description:'Handsäge' },
+    { id: 3, name: 'Hammer', description:'Zimmermannshammer' }
+];
