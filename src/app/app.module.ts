993c0367d4b2f476c34515028fc6c5a342b20f77
BobFlop <BobFlop@users.noreply.github.com>
2018-11-04 15:59:31 +0100

    created screens for appointments, appointment-detail and service completion
    screen for appointments is filled with example data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarehouseViewComponent } from './warehouse-view/warehouse-view.component';
 import { NewOrderComponent } from './new-order/new-order.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { AppointmentDetailsComponent } from './appointment-details/appointment-details.component';
+import { ServiceCompletionComponent } from './service-completion/service-completion.component';
 
 const appRoutes: Routes = [
   { path: 'warehouse', component: WarehouseViewComponent },
-  { path: 'neworder', component: NewOrderComponent }
+  { path: 'neworder', component: NewOrderComponent },
+  { path: 'appointments', component: AppointmentsComponent },
+  { path: 'appointmentdetails', component: AppointmentDetailsComponent },
+  { path: 'servicecompletion', component: ServiceCompletionComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     WarehouseViewComponent,
-    NewOrderComponent
+    NewOrderComponent,
+    AppointmentsComponent,
+    AppointmentDetailsComponent,
+    ServiceCompletionComponent
   ],
   imports: [
     BrowserModule,
