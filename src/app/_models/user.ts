1e3a34c80bbdb4dfcd06f82c0b8f2d43f53e8207
marle <marleen.eisenacht@gmx.de>
2018-12-02 15:24:09 +0100

    #23 update technician/ user characertistics and update login/ register

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,8 +1,8 @@
 export class User {
     id: number;
-    username: string;
     password: string;
-    firstName: string;
-    lastName: string;
+    name: string;
+    email: string;
+    phone: string;
     token: string;
 }
