a16baf9c6445a7cdf96081e31b573efa0ec6e442
and1kl <andreasklaiber@web.de>
2018-12-02 23:06:12 +0100

    pipeline test

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,9 +27,9 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     //browsers: ['Chrome'],
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome_without_security'], 
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+      Chrome_without_security: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
