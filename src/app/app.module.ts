52a22cff5d53c10b3022d6db92002187f72f51f7
marle <marleen.eisenacht@gmx.de>
2018-12-02 19:47:42 +0100

    update status for taskNote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { WarehouseViewComponent } from './warehouse-view/warehouse-view.component';
@@ -10,8 +10,7 @@ import { AppointmentDetailsComponent } from './appointment-details/appointment-d
 import { ServiceCompletionComponent } from './service-completion/service-completion.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
-
-import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // used to create fake backend
@@ -46,9 +45,10 @@ import { TasksNotesComponent } from './tasks-notes/tasks-notes.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
+    HttpModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     routing
   ],
   exports: [],
