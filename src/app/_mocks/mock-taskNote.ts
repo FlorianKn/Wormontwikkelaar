e128d6d3f3678bb141f847ad632aeee0b0550456
and1kl <andreasklaiber@web.de>
2019-01-27 18:22:06 +0100

    task mock data modified

--- src/app/_mocks/mock-taskNote.ts
+++ src/app/_mocks/mock-taskNote.ts
@@ -4,52 +4,22 @@ export const Status: String[] = ['Open', 'Finished'];
 export const Type: String[] = ['Task', 'Note'];
 
 export const TasksNotes: TaskNote[] = [
-    { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
+    { title: 'Befestigung Regal', description: 'Herr Müllers Regal ist um 20cm zu lange und muss gekürzt werden.', type: Type[0],
       creationDate: new Date(2018, 11, 24, 10, 33), status: Status[0]},
-     { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[1],
+     { title: 'Rückruf Chef', description: 'Rücksprache mit Chef zwecks Mittagstermin.', type: Type[1],
       creationDate: new Date(2018, 10, 12, 10, 5), status: Status[0]},
-     { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
+     { title: 'Tägliche Yoga Übung', description: 'Nicht vergessen!', type: Type[0],
       creationDate: new Date(2018, 12, 2, 13, 37), status: Status[0]},
-     { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[1],
+     { title: 'Urlaub eintragen', description: 'Urlaub für den Sommer einreichen. 20.-27. Juli.', type: Type[1],
       creationDate: new Date(2018, 11, 30, 8, 36), status: Status[0]},
-     { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
+     { title: 'Lager reparieren lassen', description: 'Lager in Fahrzeug reparieren lassen. Schloss defekt.', type: Type[0],
       creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
+      { title: 'TÜV Termin Servicefahrzeug', description: '', type: Type[0],
       creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
+      { title: 'Rückruf Herr Müller', description: '', type: Type[0],
       creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
-      creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
-      creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
-      creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
-      creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
-      creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-      + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
+      { title: 'Chef anrufen', description: 'Termin für Jahresgespräch ausmachen.', type: Type[0],
        creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-       { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
-      creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
-      creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]},
-      { title: 'Lorem ipsum dolor sit amet, consectetuer', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-     + 'Aenean commodo ligula eget dolor. Aenean massa.', type: Type[0],
+       { title: 'Memo an mich selbst', description: 'Neue Staffel GOT!', type: Type[0],
       creationDate: new Date(2018, 10, 10, 10, 10), status: Status[1]}
 ];
