3b9319dc4e6d4fbe87a07bfd961f9ad244617b13
and1kl <andreasklaiber@web.de>
2019-01-27 17:40:23 +0100

    routing to customer page modified. Parameter will be passed to open selected customer

--- src/app/_components/customers/customers.component.ts
+++ src/app/_components/customers/customers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../_models/data.service'; // for REST Api
 import { Observable } from 'rxjs';
 import { Geolocation } from '../../_models/geolocation';
@@ -14,14 +15,28 @@ export class CustomersComponent implements OnInit {
   customers: Customer[] = Customers;
   selectedCustomer: Customer;
 
-  constructor(private data: DataService) { }
+
+  // constructor(private data: DataService) { }
+  // inject the activatated route
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      console.log(params.get('customerId'));
+      this.selectedCustomer = this.getCustomerById(+params.get('customerId'));
+    });
+
+    // this.selectedCustomer = this.getCustomerById(this.route.snapshot.params.customerId);
   }
 
   onSelect(customer: Customer): void {
     delete this.selectedCustomer;
     this.selectedCustomer = customer;
+  }
+
+  getCustomerById(id: number): Customer {
+    const index = this.customers.findIndex(customer => customer.id === id);
+    return Customers[index];
   }
 
 }
